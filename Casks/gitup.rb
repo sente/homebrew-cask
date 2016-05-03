95fa81f468e265b13519935ef9b9afd5d80e32a8
Stuart Powers <stuart.powers@gmail.com>
2016-05-02 20:24:46 -0400

    Update gitup.rb
    Version increase

--- Casks/gitup.rb
+++ Casks/gitup.rb
@@ -1,6 +1,6 @@
 cask 'gitup' do
-  version '1.0.4'
-  sha256 '756222e40bb125e5e0e3231b7a5ada035c73a0461e67ce09475fb0324dd7de76'
+  version '1.0.5'
+  sha256 'ca230886c4e808518b918530392ef2eb6aea5d11dcf4ea23a89a271d3d0ee3f1'
 
   # s3-us-west-2.amazonaws.com/gitup-builds was verified as official when first introduced to the cask
   url 'https://s3-us-west-2.amazonaws.com/gitup-builds/stable/GitUp.zip'
